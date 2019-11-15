226bf46bbea576e8cdffe6d820d5e13de448e317
Alexandre Lima <limaalexguitar@gmail.com>
2019-11-15 17:39:01 -0300

    Acrescentando assinante e tamanho varchar

--- trabalho-2-parte/create.sql
+++ trabalho-2-parte/create.sql
@@ -1,32 +1,32 @@
 CREATE TABLE categoria
 (id_categoria NUMBER CONSTRAINT pk_categoria PRIMARY KEY,
-nome VARCHAR2(10)
+nome VARCHAR2(20)
 );
 
 CREATE TABLE narrador
 (id_narrador NUMBER CONSTRAINT pk_narrador PRIMARY KEY,
-nome VARCHAR2(10)
+nome VARCHAR2(40)
 );
 
 CREATE TABLE audiobook
 (id_audiobook NUMBER CONSTRAINT pk_audiobook PRIMARY KEY,
-idioma VARCHAR2(10),
+idioma VARCHAR2(15),
 duracao NUMBER
 );
 
 CREATE TABLE autor
 (id_autor NUMBER CONSTRAINT pk_autor PRIMARY KEY,
-nome VARCHAR2(10)
+nome VARCHAR2(40)
 );
 
 CREATE TABLE usuario
 (id_usuario NUMBER CONSTRAINT pk_usuario PRIMARY KEY,
-nome VARCHAR2(10),
+nome VARCHAR2(40),
 data_nascimento DATE,
-email VARCHAR2(10) CONSTRAINT unique_usuario UNIQUE,
-pais VARCHAR2(10),
-telefone VARCHAR2(10),
-senha VARCHAR2(10)
+email VARCHAR2(30) CONSTRAINT unique_usuario UNIQUE,
+pais VARCHAR2(15),
+telefone VARCHAR2(20),
+senha VARCHAR2(20)
 );
 
 CREATE TABLE pertence
@@ -55,6 +55,13 @@ id_audiobook NUMBER,
 CONSTRAINT pk_escreveu PRIMARY KEY(id_autor, id_audiobook)
 );
 
+CREATE TABLE assinante
+ (id_usuario NUMBER CONSTRAINT pk_assinante PRIMARY KEY,
+ cartao_credido VARCHAR(20),
+ data_inicio_assinatura DATE,
+ dia_cobranca NUMBER
+);
+
 ALTER TABLE pertence ADD
 (CONSTRAINT fk_pertence_id_categoria FOREIGN KEY(id_categoria) REFERENCES categoria,
 CONSTRAINT fk_pertence_id_audiobook FOREIGN KEY(id_audiobook) REFERENCES audiobook);
@@ -70,6 +77,9 @@ CONSTRAINT fk_escreveu_id_autor FOREIGN KEY(id_autor) REFERENCES autor);
 ALTER TABLE ouviu add
 (CONSTRAINT fk_ouviu_id_audiobook FOREIGN KEY(id_audiobook) REFERENCES audiobook,
 CONSTRAINT fk_ouviu_id_usuario FOREIGN KEY(id_usuario) REFERENCES usuario);
+
+ALTER TABLE assinante ADD
+(CONSTRAINT fk_assinante_id_usuario FOREIGN KEY (id_usuario) REFERENCES usuario);
 
 CREATE SEQUENCE s_categoria NOCACHE;
 
